66103f1d03d5c89b15803a524b9d54f8a295a178
zDimaBY <zdimaby@gmail.com>
2021-01-21 23:34:40 +0200

    added images and fonts

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let project_folder="docs";
 let source_folder="app";
 
 let path={
-  build:{
+  docs:{
     html: project_folder+"/",
     css: project_folder+"/css/",
     js: project_folder+"/js/",
@@ -16,7 +16,7 @@ let path={
     css: source_folder+"/css/style.css",
     js: source_folder+"/js/script.js",
     img: source_folder+"/img/**/*.{jpg,png,svg,gif,ico,webp}",
-    fonts:source_folder+"/fonts/*.ttf",
+    fonts:source_folder+"/fonts/**/*.ttf",
   },
   watch:{
     html: source_folder+"/**/*.html",
@@ -36,8 +36,12 @@ let {src, dest}=require('gulp'),
   autoprefixer=require("gulp-autoprefixer"),
   group_media=require("gulp-group-css-media-queries"),
   clean_css=require("gulp-clean-css"),
-  imagemin = require("gulp-imagemin"),
-  uglify = require("gulp-uglify-es").default;
+  imagemin=require("gulp-imagemin"),
+  webp=require("gulp-webp"),
+  webphtml=require("gulp-webp-html"),
+  ttf2woff = require("gulp-ttf2woff"),
+  ttf2woff2 = require("gulp-ttf2woff2"),
+  uglify=require("gulp-uglify-es").default;
 
 function browserSync(params){
   browsersync.init({
@@ -52,7 +56,8 @@ function browserSync(params){
 function html(){
   return src(path.app.html)
     .pipe(fileinclude())
-    .pipe(dest(path.build.html))
+    .pipe(webphtml())
+    .pipe(dest(path.docs.html))
     .pipe(browsersync.stream())
 }
 function css(){
@@ -60,39 +65,62 @@ function css(){
     .pipe(scss({outputStyle:"expanded"}))
     .pipe(autoprefixer({ooverrideBrowserslist: ["last 5 versions"],cascade: true}))
     .pipe(group_media())
-    .pipe(dest(path.build.css))
+    .pipe(dest(path.docs.css))
     .pipe(clean_css())
     .pipe(rename({extname: ".min.css"}))
-    .pipe(dest(path.build.css))
+    .pipe(dest(path.docs.css))
     .pipe(browsersync.stream())
 }
 function js(){
   return src(path.app.js)
     .pipe(fileinclude())
-    .pipe(dest(path.build.js))
+    .pipe(dest(path.docs.js))
     .pipe(uglify())
     .pipe(rename({extname: ".min.js"}))
-    .pipe(dest(path.build.js))
+    .pipe(dest(path.docs.js))
     .pipe(browsersync.stream())
 }
 function img(){
   return src(path.app.img)
-    .pipe(dest(path.build.img))
-    .pipe(browsersync.stream())
+  .pipe(webp({quality:70}))
+  .pipe(dest(path.docs.img))
+  .pipe(src(path.app.img))
+  .pipe(imagemin({progressive: true,
+    svgoPlugins:[{removeViewBox: false}],
+    interlaced: true,
+    optimizationLevel: 3}))
+  .pipe(dest(path.docs.img))
+  .pipe(browsersync.stream())
 }
+function fonts(){
+  src(path.app.fonts)
+  .pipe(ttf2woff())
+  .pipe(dest(path.docs.fonts))
+  return src(path.app.fonts)
+  .pipe(ttf2woff2())
+  .pipe(dest(path.docs.fonts));
+}
+gulp.task('svg_sprite', function() {
+  return gulp.src([source_folder + '/iconsprite/*.svg'])
+      .pipe(svg_sprite({mode:{stack: {sprite: "../icons/icons.svg",
+        example: true}}}))
+      .pipe(dest(path.docs.img))})
 function watchFiles(params){
   gulp.watch([path.watch.html],html)
   gulp.watch([path.watch.css],css)
   gulp.watch([path.watch.js],js)
+  gulp.watch([path.watch.img],img)
 }
 function clean(params){
   return del(path.clean);
 }
-let build=gulp.series(clean, gulp.parallel(css, js, html));
-let watch=gulp.parallel(watchFiles,build,browserSync);
+let docs=gulp.series(clean, gulp.parallel(css, js, html, img, fonts));
+let watch=gulp.parallel(watchFiles,docs,browserSync);
 
+exports.fonts=fonts;
+exports.img=img;
 exports.scss=scss;
 exports.html=html;
-exports.build=build;
+exports.docs=docs;
 exports.watch=watch;
 exports.default=watch;
