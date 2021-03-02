b851acd2a70ff749e3125dfda4aa7bef743f6ec3
zDimaBY <zdimaby@gmail.com>
2021-03-02 17:06:32 +0200

    opencart3_v2.1

--- catalog/controller/extension/module/featureddown.php
+++ catalog/controller/extension/module/featureddown.php
@@ -74,8 +74,15 @@ class ControllerExtensionModuleFeatureddown extends Controller {
 						'special'     => $special,
 						'tax'         => $tax,
 						'rating'      => $rating,
+						'sale'		  => round(100-(int)$special*100/(int)$price),
 						'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
 					);
+					if (isset($data['product_special'])) {
+						foreach ($data['product_special'] as $product_special) {
+							$this->db->query("INSERT INTO " . DB_PREFIX . "product_special SET product_id = '" . (int)$product_id . "', customer_group_id = '" . (int)$product_special['customer_group_id'] . "', priority = '" . (int)$product_special['priority'] . "', price = '" . (float)$product_special['price'] . "', date_start = '" . $this->db->escape($product_special['date_start']) . "', date_end = '" . $this->db->escape($product_special['date_end']) . "'");
+						}
+					}
+			
 				}
 			}
 		}
